66f432080873ccc122acab1d3d2902b58b33fb47
Phil B <muppetbrown@gmail.com>
2026-01-20 10:57:25 +1300

    thumbnail fix

--- backend/alembic/versions/remove_thumbnail_columns.py
+++ backend/alembic/versions/remove_thumbnail_columns.py
@@ -17,7 +17,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'pending_remove_thumbnails'
-down_revision = '<latest_revision_id>'  # UPDATE THIS before running
+down_revision = 'f37582e531b4'  # UPDATE THIS before running
 branch_labels = None
 depends_on = None
 
